f56284434d55b398c1a4f4369db84d2da792bbb8
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-09 22:14:14 +0300

    setting up for the JWT authentication

--- laravel-server/app/Models/User.php
+++ laravel-server/app/Models/User.php
@@ -6,8 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable{
+class User extends Authenticatable implements JWTSubject{
     HasFactory, Notifiable;
 
     /**
@@ -50,5 +51,23 @@ class User extends Authenticatable{
         return $this->belongsToMany(User::class)
         ->as('likes')
         ->withTimestamps();
+    }
+    
+     /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims(){
+        return [];
     }
 }
